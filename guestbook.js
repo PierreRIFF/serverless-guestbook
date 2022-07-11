d11e0bfa6f9cfd06152861334344ac0158d1f3a1
PierreRIFF <31289325+PierreRIFF@users.noreply.github.com>
2022-07-11 13:15:34 +1000

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${https://af2869d8.au-syd.apigw.appdomain.cloud/guestbook}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
